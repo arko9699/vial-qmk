fac590f0cc2722b1433241435e096162af98d223
Arkaprobho Das <89677542+arko9699@users.noreply.github.com>
2025-12-01 18:19:33 +0530

    just some stuff
    Added definitions for bonk R2 and renamed path for R1.
    Also added vial support for maelk's mw60s

---
+++ keyboards/arko/bonkr2/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE1, 0xA6, 0x13, 0xED, 0x2C, 0xA2, 0xA0, 0xDD}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
---
+++ keyboards/arko/maelk_mw60/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x66, 0xF1, 0xFC, 0xF3, 0x99, 0x93, 0xD1, 0xBA}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
